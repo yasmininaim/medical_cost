110bf3b42f10f2e214a6976b8fcc2cf402f51112
yasmininaim <yasmina.h.naimzade@gmail.com>
2025-01-04 19:05:36 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('Medical cost')
 
 st.write('Hello world!')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
