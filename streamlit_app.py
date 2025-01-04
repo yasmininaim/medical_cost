644c7f22451f1077eaba67459de7baa629a63f01
yasmininaim <yasmina.h.naimzade@gmail.com>
2025-01-04 19:26:25 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,13 @@ st.dataframe(y_raw)
 with st.sidebar:
   st.header("Введите признаки: ")
   island = st.selection('Island', ('Torgersen', 'Dream', 'Biscoe'))
+  bill_lenght_mm = st.slider('Bill length (mm)', 32.1, 59.6, 44,5)
+  bill_lenght_mm = st.slider('Bill length (mm)', 13.1, 21.5, 17,3)  
+  flipper_lenght_mm = st.slider('Flipper length (mm)', 32.1, 59.6, 44,5)  
+  body_mass_g = st.slider('Body mass (g)', 32.1, 59.6, 44,5)
+  gender = st.selectbox('Gender', ('female', 'male'))
+
+
+
+
+
