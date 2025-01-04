55d97def7c3ad7ae3347932f7a38a8ccd48f1ff4
yasmininaim <yasmina.h.naimzade@gmail.com>
2025-01-04 19:14:21 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,13 @@ st.title('Medical cost')
 st.write('Hello world!')
 
 df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+
+with st.epander ('Data'):
+  st.write("X")
+  X_raw = df.drop('', axis=1)
+
+st.dataframe(X_raw)
+
+st.write ("y")
+y_raw = df.species
+st.dataframe(y_raw)
